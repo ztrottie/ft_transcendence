1887332cb92380c42b4496460b1ec266ada588db
ztrottie <zakytrottier@hotmail.fr>
2024-04-24 20:15:40 -0400

    test de test tester

--- buildTest.sh
+++ buildTest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sleep 180
 running="$(docker-compose -f "./srcs/docker-compose-test.yml" -p transcendence-test ps --services --filter "status=running")"
 services="$(docker-compose -f "./srcs/docker-compose-test.yml" -p transcendence-test ps --services)"
 if [ "$running" != "$services" ]; then
