6b4bc2f147918aa53e05037ab751cd2fecfad744
olivier boucher <olivierboucher@mbp-de-olivier.home>
2024-05-16 20:15:30 -0400

    add cub

--- srcs/web/www/Game/main.js
+++ srcs/web/www/Game/main.js
@@ -1,3 +1,35 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const scene = THREE.scene();
+const scene = new THREE.Scene();
+
+let camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("three").appendChild(renderer.domElement);
+
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
