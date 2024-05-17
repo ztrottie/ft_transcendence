1e06f6180f254d395b3ed22680ea18cabb987e9a
axhar005 <olivier.boucher.game@hotmail.com>
2024-05-17 16:11:09 -0400

    update

--- srcs/web/www/Game/main.js
+++ srcs/web/www/Game/main.js
@@ -1,34 +1,38 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
-let camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 8;
-
-let renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("three").appendChild(renderer.domElement);
 
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  200
+);
 
+const orbit = new OrbitControls(camera, renderer.domElement );
 const axesHelper = new THREE.AxesHelper(5);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+camera.position.set(10, 2, 5);
+orbit.update();
+
+
 scene.add(axesHelper);
-
-let geometry = new THREE.BoxGeometry();
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-let cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-
+cube.position.x = 2;
 
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+  orbit.update();
   renderer.render(scene, camera);
 }
 
