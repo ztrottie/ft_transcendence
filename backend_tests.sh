8b5262db96eef3ce62007bbe2e954a546c0c8933
root <root@ZACK-PORTABLE>
2024-05-20 21:35:10 -0400

    done wibackend testing should work properly

--- backend_tests.sh
+++ backend_tests.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 docker container exec backend python manage.py test --noinput
-exit $?
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -eq 0 ]; then
+  echo "all tests passed successfully"
+elif [ $EXIT_STATUS -eq 1 ]; then
+  echo "at least one test failed, please correct it"
+else
+  echo "there is a problem in the tests code, please correct it"
+fi
+exit $EXIT_STATUS
