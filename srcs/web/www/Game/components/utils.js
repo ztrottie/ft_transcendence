b2da57c4f3f910cc91de6471cba2a9f80a175025
axhar005 <olivier.boucher.game@hotmail.com>
2024-09-23 10:57:54 -0400

    fix setPlayerNmae

--- srcs/web/www/Game/components/utils.js
+++ srcs/web/www/Game/components/utils.js
@@ -5,8 +5,8 @@ export function setProperties(game, life, round, gameMode){
 }
 
 export function setPlayerName(game, name1, name2, name3, name4){
-	game.playerName[0].name = name1;
-	game.playerName[1].name = name2;
-	game.playerName[2].name = name3;
-	game.playerName[3].name = name4;
+	game.playerName[0] = name1;
+	game.playerName[1] = name2;
+	game.playerName[2] = name3;
+	game.playerName[3] = name4;
 }
